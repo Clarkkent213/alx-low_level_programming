531cf41ca8ef66686054f0233cea36d8a374526b
Clark <clrkconstable@gmail.com>
2023-02-22 06:28:39 -0800

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@ i#include <stdio.h>
 
 void print_remaining_days(int month, int day, int year)
 {
-while (year % 4 == 0 || (year % 400 == 0 && (year % 100 == 0))
+while (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 {
 if (month > 2 && day >= 60)
 {
