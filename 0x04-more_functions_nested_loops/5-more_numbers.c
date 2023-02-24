ebf6f747e0d67824a04da23113b3258853fea9c0
Clark <clrkconstable@gmail.com>
2023-02-24 00:35:45 -0800

    more fuctions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,18 @@
  */
 void more_numbers(void)
 {
-        int i, ro;
+	int i, ro;
 
-        for (ro = 0; ro < 10; ro++)
-        {
-                for (i = 0; i <= 14; i++)
-                {
-                        if (i >= 10)
-                        {
-                                _putchar((i / 10) + '0');
-                        }
-                        _putchar((i % 10) + '0');
-                }
-                _putchar('\n');
-        }
+	for (ro = 0; ro < 10; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
